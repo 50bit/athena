2034ffb1cbf7338b82fe57723465013dd70968fd
50bit <omar.gamal1015@gmail.com>
2024-01-08 22:42:29 +0200

    add auth to headers

--- src/axios.js
+++ src/axios.js
@@ -1,13 +1,13 @@
 import axios from 'axios';
 
 const BASE_URL = "http://173.249.60.28:60772/api";
-const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFobGFsbGFoLmNvbSIsImdlbmRlciI6Im1hbGUiLCJqdGkiOiIzODE0Y2FjMS1iMjZjLTQ2OTYtYmRiMi04MzBiMzNiYmM3NTgiLCJleHAiOjE3MTMwMzkxMzgsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NjA3NzIiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjQyMDAifQ.iFUI2YF721YM0lCmtB96W7ed5vnggFq6GqvcftCC-tw'
+// const TOKEN = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJlbWFpbCI6ImFkbWluQGFobGFsbGFoLmNvbSIsImdlbmRlciI6Im1hbGUiLCJqdGkiOiIzODE0Y2FjMS1iMjZjLTQ2OTYtYmRiMi04MzBiMzNiYmM3NTgiLCJleHAiOjE3MTMwMzkxMzgsImlzcyI6Imh0dHA6Ly9sb2NhbGhvc3Q6NjA3NzIiLCJhdWQiOiJodHRwOi8vbG9jYWxob3N0OjQyMDAifQ.iFUI2YF721YM0lCmtB96W7ed5vnggFq6GqvcftCC-tw'
 const axiosInstance = axios.create({
     headers: {
         'Access-Control-Allow-Origin': '*',
         common: {
-            Authorization: `Bearer ${TOKEN}`
-            // Authorization: ``
+            // Authorization: `Bearer ${TOKEN}`
+            Authorization: ``
         }
     },
     baseURL: BASE_URL
@@ -20,7 +20,8 @@ const axiosInstance = axios.create({
 axiosInstance.interceptors.request.use(request => {
     // console.log(request);
     // Edit request config
-    // request.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
+    if(localStorage.getItem('token'))
+        request.headers.Authorization = `Bearer ${localStorage.getItem('token')}`;
     return request;
 }, error => {
     console.log(error);
